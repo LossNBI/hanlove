0a0541c2834982b7bfff87d858f92f0e0944d1e8
LossNBI <Jisung@5e604d9b56f1>
2025-08-23 06:10:13 +0000

    공지사항 기능 구현 완료 및 버그 수정

--- notice/urls.py
+++ notice/urls.py
@@ -1,12 +1,13 @@
-# hanlove_final/notice/urls.py
+# hanlove/notice/urls.py
 
 from django.urls import path
 from .views import NoticeListCreateView, NoticeDetailView
 
 urlpatterns = [
-    # 공지사항 목록 조회 및 생성 (POST 요청 시 관리자만 가능)
-    path('notices/', NoticeListCreateView.as_view(), name='notice-list-create'),
-    # 특정 공지사항 삭제 (DELETE 요청 시 관리자만 가능)
-    path('notices/<int:pk>/', NoticeDetailView.as_view(), name='notice-detail'),
+    # 공지사항 목록 조회 (GET)
+    path('list/', NoticeListCreateView.as_view(), name='notice-list'),
+    # 공지사항 생성 (POST)
+    path('create/', NoticeListCreateView.as_view(), name='notice-create'),
+    # 특정 공지사항 상세 조회, 업데이트, 삭제
+    path('<int:pk>/', NoticeDetailView.as_view(), name='notice-detail'),
 ]
-
