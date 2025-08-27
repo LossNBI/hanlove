06554cd9c25e12ea954cb1f846710e66dc85c9fa
LossNBI <son2717@gmail.com>
2025-08-27 15:41:01 +0900

    feat: add video upload functionality

--- posts/models.py
+++ posts/models.py
@@ -1,16 +1,25 @@
 # posts/models.py
 
 from django.db import models
-# 변경! Django 기본 User 모델 대신, 직접 만든 CustomUser를 가져옵니다.
-from users.models import CustomUser 
+from django.conf import settings
 
 class Post(models.Model):
-    # 변경! ForeignKey가 CustomUser를 참조하도록 합니다.
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
-    caption = models.TextField()
-    image = models.ImageField(upload_to='posts/images/', null=True, blank=True)
-    video = models.FileField(upload_to='posts/videos/', null=True, blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
+    caption = models.TextField(blank=True)
+    # 이미지 파일을 저장하기 위한 ImageField
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    # 동영상 파일을 저장하기 위한 FileField 추가
+    video = models.FileField(upload_to='post_videos/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Post by {self.author.username} at {self.created_at}'
+        return f'{self.author.username} - {self.caption[:30]}'
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post}'
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,23 +1,19 @@
 # posts/serializers.py
 
 from rest_framework import serializers
-from .models import Post
-from users.models import CustomUser
+from .models import Post, Comment
 
-# 게시물 작성자의 닉네임을 보여주기 위한 Serializer
-class CustomUserSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
     class Meta:
-        model = CustomUser
-        fields = ['nickname']
+        model = Comment
+        fields = ['id', 'author', 'text', 'created_at']
 
-# Post 모델을 위한 Serializer
 class PostSerializer(serializers.ModelSerializer):
-    # 'author' 필드를 읽기 전용으로 설정하고,
-    # 중첩된 Serializer를 사용해 닉네임을 포함시킵니다.
-    author = CustomUserSerializer(read_only=True) 
+    author = serializers.ReadOnlyField(source='author.username')
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        # 'author' 필드는 자동 생성되므로, 리스트에 포함시키지 않습니다.
-        fields = ['id', 'author', 'caption', 'image', 'video', 'created_at']
-        read_only_fields = ['id', 'author', 'created_at']
+        fields = ['id', 'author', 'caption', 'image', 'video', 'created_at', 'comments']
