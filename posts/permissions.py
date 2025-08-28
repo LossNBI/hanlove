20d367c2654d9293b174eb22885e96c8a08e9257
LossNBI <son2717@gmail.com>
2025-08-28 09:49:31 +0900

    feat: 댓글 권한 클래스 추가

--- posts/permissions.py
+++ posts/permissions.py
@@ -2,26 +2,27 @@
 
 from rest_framework import permissions
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    """
-    게시물의 작성자만 수정 및 삭제를 허용하는 커스텀 권한
-    """
-    def has_object_permission(self, request, view, obj):
-        # 읽기 요청(GET, HEAD, OPTIONS)은 모든 사용자에게 허용합니다.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # 쓰기 요청(PUT, PATCH, DELETE)은 작성자만 허용합니다.
-        return obj.author == request.user
-
 class IsAuthorOrAdmin(permissions.BasePermission):
     """
-    작성자 또는 관리자만 수정 및 삭제를 허용하는 커스텀 권한
+    게시물 작성자이거나 관리자(is_staff)일 때만 수정/삭제를 허용하는 커스텀 권한
     """
     def has_object_permission(self, request, view, obj):
-        # 읽기 요청(GET, HEAD, OPTIONS)은 모든 사용자에게 허용합니다.
+        # 읽기 권한은 모두에게 허용 (GET, HEAD, OPTIONS)
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # 쓰기 요청(PUT, PATCH, DELETE)은 작성자 또는 관리자만 허용합니다.
-        return obj.author == request.user or request.user.is_superuser
+        # 수정 및 삭제 권한은 게시물 작성자이거나 관리자일 때만 허용
+        # is_staff는 Django Admin에서 관리자 권한을 가진 사용자에게만 True입니다.
+        return obj.author == request.user or request.user.is_staff
+
+class IsCommentAuthorOrAdmin(permissions.BasePermission):
+    """
+    댓글 작성자이거나 관리자일 때만 수정/삭제를 허용하는 커스텀 권한
+    """
+    def has_object_permission(self, request, view, obj):
+        # 읽기 권한은 모두에게 허용 (GET, HEAD, OPTIONS)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # 수정 및 삭제 권한은 댓글 작성자이거나 관리자일 때만 허용
+        return obj.author == request.user or request.user.is_staff
