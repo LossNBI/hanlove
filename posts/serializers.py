2ad8657761c10a5cc63cb9e43751cf430b4e1815
LossNBI <son2717@gmail.com>
2025-08-28 09:30:39 +0900

    feat: 게시물 수정/삭제 권한 추가2

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,18 +2,26 @@
 
 from rest_framework import serializers
 from .models import Post, Comment
+from users.serializers import CustomUserSerializer # CustomUserSerializer import
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
-
+    author = serializers.ReadOnlyField(source='author.nickname')
+    
     class Meta:
         model = Comment
         fields = ['id', 'author', 'text', 'created_at']
 
-class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
-    comments = CommentSerializer(many=True, read_only=True)
 
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.nickname')
+    comments = CommentSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Post
         fields = ['id', 'author', 'caption', 'image', 'video', 'created_at', 'comments']
+        
+    # author_id 필드 추가
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['author_id'] = instance.author.id
+        return representation
